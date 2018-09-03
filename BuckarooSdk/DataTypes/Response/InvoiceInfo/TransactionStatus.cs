b60943297ac3576fa231440f978eb5fdbcdf8fc8
João Velasques <j.velasques@mindbus.nl>
2018-09-03 17:45:08 +0100

    Using the TransactionStatus Model instead of string. Setting TransactionInvoiceInfoBase in the MultipleInvoicesInfo constructor.

--- BuckarooSdk/DataTypes/Response/InvoiceInfo/TransactionStatus.cs
+++ BuckarooSdk/DataTypes/Response/InvoiceInfo/TransactionStatus.cs
@@ -4,7 +4,7 @@ namespace BuckarooSdk.DataTypes.Response.InvoiceInfo
 {
 	public class TransactionStatus
 	{
-		public bool Succes { get; set; }
+		public bool Success { get; set; }
 		public string Code { get; set; }
 		public string Message { get; set; }
 		public DateTime DateTime { get; set; }
