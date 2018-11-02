20c61543ebd5ff492b9580881fc7de28b7d075f1
Buckaroo <techsup@buckaroo.nl>
2018-11-02 11:22:25 +0100

    Updated iDEAL classes.
    updated the iDEAL classes with some extended documentation and altered parameters.

--- BuckarooSdk/Services/Ideal/TransactionRequest/IdealRefundRequest.cs
+++ BuckarooSdk/Services/Ideal/TransactionRequest/IdealRefundRequest.cs
@@ -1,23 +1,12 @@
 ﻿
 namespace BuckarooSdk.Services.Ideal.TransactionRequest
 {
-    /// <summary>
-    /// The ideal refund request class, where customer values of the request can be specified.
-    /// </summary>
-    public class IdealRefundRequest
+	/// <summary>
+	///	To refund a successfully performed iDEAL transaction, use the action Refund. This is only available for ideal collecting. 
+	/// A bank transfer is then initiated which transfers the amount of the original transaction to the consumer. 
+	/// </summary>
+	public class IdealRefundRequest
     {
-        /// <summary>
-        /// The account name of the customer.
-        /// </summary>
-        public string CustomerAccountName { get; set; }
-        /// <summary>
-        /// The BIC (bank) code of the customer's account, where the refund is meant for.
-        /// </summary>
-        public string CustomerBic { get; set; }
-        /// <summary>
-        /// The IBAN of the customer's account, where the refund is meant for.
-        /// </summary>
-        public string CustomerIban { get; set; }
-
+        
     }
 }
