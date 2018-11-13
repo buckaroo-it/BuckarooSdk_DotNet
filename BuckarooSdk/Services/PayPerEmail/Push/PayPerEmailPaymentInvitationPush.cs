1813142e84ca7709617619eddb0bc2500dcc49b2
sjaak-buckaroo <s.roos@buckaroo.nl>
2018-11-13 16:04:39 +0100

    Added the PPE actionpush and made a small refactor modification to the push class

---
+++ BuckarooSdk/Services/PayPerEmail/Push/PayPerEmailPaymentInvitationPush.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuckarooSdk.Services.PayPerEmail.Push
+{
+	public class PayPerEmailPaymentInvitationPush : ActionResponse
+	{
+		public override ServiceEnum ServiceEnum => ServiceEnum.PayPerEmail;
+		public DateTime ExpirationDate { get; set; }
+		public string PayLink { get; set; }
+	}
+}
