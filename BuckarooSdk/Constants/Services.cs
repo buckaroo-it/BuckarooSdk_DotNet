da42a207dfc668206254c2a38b8c4f959a7f5c52
Sjaak Roos <s.roos@buckaroo.nl>
2020-01-31 15:36:07 +0100

    added huis en tuin giftcard pushes.

--- BuckarooSdk/Constants/Services.cs
+++ BuckarooSdk/Constants/Services.cs
@@ -47,6 +47,7 @@
 			BuckarooVoucher,
 			Notification,
 			Ippies,
+			HuisTuinGiftcard,
 		}
 	}
 }
---
+++ BuckarooSdk/Services/Giftcards/HuisTuinGiftcard/HuisTuinGiftcardRefundPush.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static BuckarooSdk.Constants.Services;
+
+namespace BuckarooSdk.Services.Giftcards.HuisTuinGiftcard
+{
+	public class HuisTuinGiftcardRefundPush : ActionPush
+	{
+		public override ServiceNames ServiceNames => ServiceNames.HuisTuinGiftcard;
+	}
+}
