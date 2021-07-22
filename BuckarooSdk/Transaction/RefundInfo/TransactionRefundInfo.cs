058dd3d9319cb4adc61ecfe98b35a9e5906dc8ac
Tim van den Hof <tim.site@gmail.com>
2021-07-22 22:11:33 +0200

    Fix: IDE0060 Remove unused parameter 'transaction' if it is not part of a shipped public API ==> Marked obsolete, added new constructor

--- BuckarooSdk/Transaction/RefundInfo/TransactionRefundInfo.cs
+++ BuckarooSdk/Transaction/RefundInfo/TransactionRefundInfo.cs
@@ -1,34 +1,38 @@
-﻿using BuckarooSdk.Base;
+﻿using System;
+using BuckarooSdk.Base;
 using BuckarooSdk.DataTypes.RequestBases;
 
 namespace BuckarooSdk.Transaction.RefundInfo
 {
-	public class TransactionRefundInfo
-	{
-		internal AuthenticatedRequest AuthenticatedRequest { get; set; }
-		internal TransactionRefundInfoBase TransactionRefundInfoBase { get; set; }
+    public class TransactionRefundInfo
+    {
+        internal AuthenticatedRequest AuthenticatedRequest { get; set; }
 
-		public TransactionRefundInfo(AuthenticatedRequest request)
-		{
-			this.AuthenticatedRequest = request;
-		}
+        internal TransactionRefundInfoBase TransactionRefundInfoBase { get; set; }
 
-		public ConfiguredTransactionRefundInfo SpecificConfiguredTransactionRefundInfo(string transactionKey)
-		{
-			this.AuthenticatedRequest.Request.Endpoint += ($"{Constants.Settings.GatewaySettings.TransactionRequestEndPoint}" +
-											$"{Constants.Settings.GatewaySettings.RefundInfoEndPoint}" +
-											$"{transactionKey}");
+        public TransactionRefundInfo(AuthenticatedRequest request)
+        {
+            AuthenticatedRequest = request;
+        }
 
-			return new ConfiguredTransactionRefundInfo(this);
-		}
+        public ConfiguredTransactionRefundInfo SpecificConfiguredTransactionRefundInfo(string transactionKey)
+        {
+            AuthenticatedRequest.Request.Endpoint += $"{Constants.Settings.GatewaySettings.TransactionRequestEndPoint}{Constants.Settings.GatewaySettings.RefundInfoEndPoint}{transactionKey}";
 
-		public ConfiguredTransactionRefundInfo MultipleConfiguredTransactionRefundInfo(
-			TransactionRefundInfoBase transactionRefundInfoBase)
-		{
-			this.AuthenticatedRequest.Request.Endpoint += ($"{Constants.Settings.GatewaySettings.TransactionRequestEndPoint}" +
-											$"{Constants.Settings.GatewaySettings.RefundInfosEndPoint}");
+            return new ConfiguredTransactionRefundInfo(this);
+        }
+        
+        public ConfiguredTransactionRefundInfo MultipleConfiguredTransactionRefundInfo()
+        {
+            AuthenticatedRequest.Request.Endpoint += $"{Constants.Settings.GatewaySettings.TransactionRequestEndPoint}{Constants.Settings.GatewaySettings.RefundInfosEndPoint}";
 
-			return new ConfiguredTransactionRefundInfo(this);
-		}
-	}
+            return new ConfiguredTransactionRefundInfo(this);
+        }
+
+        [Obsolete("Parameter transactionRefundInfoBase is no longer needed.")]
+        public ConfiguredTransactionRefundInfo MultipleConfiguredTransactionRefundInfo(TransactionRefundInfoBase transactionRefundInfoBase)
+        {
+            return MultipleConfiguredTransactionRefundInfo();
+        }
+    }
 }
