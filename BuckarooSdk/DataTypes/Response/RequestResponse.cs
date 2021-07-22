950b68a07baf56294ee0174e8255c69ff9d94c8d
Tim van den Hof <tim.site@gmail.com>
2021-07-22 22:21:53 +0200

    Fix: IDE0059 Unnecessary assignment of a value to 'actionError'

--- BuckarooSdk/DataTypes/Response/RequestResponse.cs
+++ BuckarooSdk/DataTypes/Response/RequestResponse.cs
@@ -21,26 +21,32 @@ namespace BuckarooSdk.DataTypes.Response
 		/// is performed.
 		/// </summary>
 		public ILogger BuckarooSdkLogger { get; internal set; }
+
 		/// <summary>
 		/// The transaction key
 		/// </summary>
 		public string Key { get; set; }
+
 		/// <summary>
 		/// The status of the transaction
 		/// </summary>
 		public Status.Status Status { get; set; }
+
 		/// <summary>
 		/// The required action
 		/// </summary>
 		public RequiredAction RequiredAction { get; set; }
+
 		/// <summary>
 		/// The list of services that were available for the transaction request
 		/// </summary>
 		public List<Service> Services { get; set; }
+
 		/// <summary>
 		/// The list of custom parameters that was sent with the transaction request
 		/// </summary>
 		public CustomParameters CustomParameters { get; set; }
+
 		/// <summary>
 		/// The list of additional parameters that was sent with the transaction request
 		/// </summary>
@@ -50,81 +56,97 @@ namespace BuckarooSdk.DataTypes.Response
 		/// An overcoupling error overview with list of all errors per error type.
 		/// </summary>
 		public RequestErrors RequestErrors { get; set; }
+
 		/// <summary>
 		/// The invoice used in the request.
 		/// </summary>
 		public string Invoice { get; set; }
+
 		/// <summary>
 		/// The code of the service used for the transaction.
 		/// </summary>
 		public string ServiceCode { get; set; }
+
 		/// <summary>
 		/// boolean that specifies if the transaction was a test transaction.
 		/// </summary>
 		public bool IsTest { get; set; }
+
 		/// <summary>
 		/// The currency that 
 		/// </summary>
 		public string Currency { get; set; }
+
 		/// <summary>
 		/// The debit amount of the requested transaction
 		/// </summary>
 		public decimal? AmountDebit { get; set; }
+
 		/// <summary>
 		/// The transaction type that was specified in the transaction request.
 		/// </summary>
 		public string TransactionType { get; set; }
+
 		/// <summary>
 		/// The mutation type of the transaction request. 
 		/// </summary>
 		public MutationType MutationType { get; set; }
+
 		/// <summary>
 		/// A list of transactions that are related to the transaction that was requested.
 		/// </summary>
 		public List<RelatedTransaction> RelatedTransactions { get; set; }
+
 		/// <summary>
 		/// The consumer message
 		/// </summary>
 		public ConsumerMessage ConsumerMessage { get; set; }
+
 		/// <summary>
 		/// The order value. This is an alphanumerical value.
 		/// </summary>
 		public string Order { get; set; }
+
 		/// <summary>
 		/// The issuing country of the requested transaction.
 		/// </summary>
 		public string IssuingCountry { get; set; }
+
 		/// <summary>
 		/// Boolean value that specifies wheter the requested transactions initiated a startrecurrent.
 		/// </summary>
 		public bool StartRecurrent { get; set; }
+
 		/// <summary>
 		/// Boolean value that specifies wheter the transaction is a recurring transaction.
 		/// </summary>
 		public bool Recurring { get; set; }
+
 		/// <summary>
 		/// The number of the customer that the transaction request was meant for.
 		/// </summary>
 		public string CustomerName { get; set; }
+
 		/// <summary>
 		/// The payer hash value of the requested transaction.
 		/// </summary>
 		public string PayerHash { get; set; }
+
 		/// <summary>
 		/// The key value of the payment.
 		/// </summary>
 		public string PaymentKey { get; set; }
 
-		public string RawRequest => this.BuckarooSdkLogger.GetRawRequest();
+		public string RawRequest => BuckarooSdkLogger.GetRawRequest();
 
-		public string RawResponse => this.BuckarooSdkLogger.GetRawResponse();
+		public string RawResponse => BuckarooSdkLogger.GetRawResponse();
 
 		/// <summary>
 		/// Primary constructor.
 		/// </summary>
 		public RequestResponse()
 		{
-			this.Status = new Status.Status();
+			Status = new Status.Status();
 		}
 
 		public List<ServiceNames> GetServices()
@@ -141,12 +163,6 @@ namespace BuckarooSdk.DataTypes.Response
 
 		public bool CheckResponseForErrors()
 		{
-			
-			foreach (var actionError in RequestErrors.ActionErrors)
-			{
-				
-			}
-
 			return false;
 		}
 
