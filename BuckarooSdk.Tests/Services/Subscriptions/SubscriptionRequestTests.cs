1d0df09db899b6e69374a9a744ed8ac4bf853093
Tim van den Hof <tim.site@gmail.com>
2021-07-22 22:12:31 +0200

    Fix: IDE0052 Private property 'SubscriptionRequestTests.BuckarooClient' can be converted to a method as its get accessor is never invoked.

--- BuckarooSdk.Tests/Services/Subscriptions/SubscriptionRequestTests.cs
+++ BuckarooSdk.Tests/Services/Subscriptions/SubscriptionRequestTests.cs
@@ -4,26 +4,23 @@ using BuckarooSdk.Tests.Constants;
 
 namespace BuckarooSdk.Tests.Services.Subscriptions
 {
-	[TestClass]
-	public class SubscriptionRequestTests
-	{
-		private SdkClient BuckarooClient { get; set; }
+    [TestClass]
+    public class SubscriptionRequestTests
+    {
+        private SdkClient buckarooClient;
 
-		[TestInitialize]
-		public void Setup()
-		{
-			this.BuckarooClient = new SdkClient(TestSettings.Logger);
-		}
+        [TestInitialize]
+        public void Setup()
+        {
+            buckarooClient = new SdkClient(TestSettings.Logger);
+        }
 
-		[TestMethod]
-		public void CreateSubscriptionTest()
-		{
-			//var request = this._buckarooClient.CreateRequest()
-			//.Authenticate(TestSettings.WebsiteKey, TestSettings.SecretKey, false, new CultureInfo("nl-NL"))
-			//.Subscriptions()
-		}
-
-
-
-	}
+        [TestMethod]
+        public void CreateSubscriptionTest()
+        {
+            //var request = this._buckarooClient.CreateRequest()
+            //.Authenticate(TestSettings.WebsiteKey, TestSettings.SecretKey, false, new CultureInfo("nl-NL"))
+            //.Subscriptions()
+        }
+    }
 }
