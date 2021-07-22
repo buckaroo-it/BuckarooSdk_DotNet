611d38ea548d22d5e6bc9c27af0dabbb8eac84d5
Tim van den Hof <tim.site@gmail.com>
2021-07-22 22:20:38 +0200

    Fix: IDE0059 Unnecessary assignment of a value to 'list2'

--- BuckarooSdk/Transaction/Status/ConfiguredTransactionStatus.cs
+++ BuckarooSdk/Transaction/Status/ConfiguredTransactionStatus.cs
@@ -1,40 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using BuckarooSdk.DataTypes;
 using BuckarooSdk.DataTypes.RequestBases;
 using BuckarooSdk.DataTypes.Response.StatusRequest;
 
 namespace BuckarooSdk.Transaction.Status
 {
-	public class ConfiguredTransactionStatus
-	{
-		internal TransactionStatus TransactionStatus { get; set; }
-		internal TransactionReference TransactionReference { get; set; }
+    public class ConfiguredTransactionStatus
+    {
+        internal TransactionStatus TransactionStatus { get; set; }
+        internal TransactionReference TransactionReference { get; set; }
 
-		internal ConfiguredTransactionStatus(TransactionStatus transactionStatus)
-		{
-			this.TransactionStatus = transactionStatus;
+        internal ConfiguredTransactionStatus(TransactionStatus transactionStatus)
+        {
+            TransactionStatus = transactionStatus;
+        }
 
-			var list = new List<string>();
-			var list2 = list.ToList();
-		}
-		
+        public StatusesRequestResponse GetMultipleStatuses()
+        {
+            if (TransactionStatus.Request.Request == null)
+            {
+                throw new Exception("This function is a POST method and should therefore contain a message body");
+            }
 
-		public StatusesRequestResponse GetMultipleStatuses()
-		{
-			if (this.TransactionStatus.Request.Request == null)
-			{
-				throw new Exception("This function is a POST method and should therefore contain a message body");
-			}
-			return Connection.Connector.SendRequest<IRequestBase, StatusesRequestResponse>
-				(this.TransactionStatus.Request.Request, this.TransactionStatus.TransactionStatusBase, HttpRequestType.Post).Result;
-		}
+            return Connection.Connector.SendRequest<IRequestBase, StatusesRequestResponse>(TransactionStatus.Request.Request, TransactionStatus.TransactionStatusBase, HttpRequestType.Post).Result;
+        }
 
-		public DataTypes.Response.StatusRequest.TransactionStatus GetSingleStatus()
-		{
-			return Connection.Connector.SendRequest<IRequestBase, DataTypes.Response.StatusRequest.TransactionStatus>
-				(this.TransactionStatus.Request.Request, this.TransactionStatus.TransactionStatusBase, HttpRequestType.Get).Result;
-		}
-	}
+        public DataTypes.Response.StatusRequest.TransactionStatus GetSingleStatus()
+        {
+            return Connection.Connector.SendRequest<IRequestBase, DataTypes.Response.StatusRequest.TransactionStatus>(TransactionStatus.Request.Request, TransactionStatus.TransactionStatusBase, HttpRequestType.Get).Result;
+        }
+    }
 }
