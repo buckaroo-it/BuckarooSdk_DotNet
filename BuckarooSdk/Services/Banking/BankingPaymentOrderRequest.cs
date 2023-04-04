57ad5aab3591d9432421476f65654fac0480e97c
RMG <r.giebels@bucakroo.nl>
2023-04-04 11:46:10 +0200

    PS2-154 - Implement Banking service
    Implement Banking service

---
+++ BuckarooSdk/Services/Banking/BankingPaymentOrderRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BuckarooSdk.Services.Banking
+{
+    public class BankingPaymentOrderRequest
+    {
+        public string StructuredIssuerType { get; set; }
+        public DateTime ProcessingDate { get; set; }
+        public string Purpose { get; set; }
+        public string IBAN { get; set; }
+        public string BIC { get; set; }
+        public string StructuredReference { get; set; }
+        public string AccountHolderName { get; set; }
+    }
+}
