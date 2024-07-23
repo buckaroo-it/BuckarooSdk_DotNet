e8f3653fda4920a665e8d0f128d100577ee2cfb4
xhanan <61253303+xhanan@users.noreply.github.com>
2024-07-23 12:55:04 +0200

    adding PayRecurrent
    PaywithEmandate
    
    Authorize
    
    ExtraInfo (this one is a secondary action), e.g. Pay, ExtraInfo

---
+++ BuckarooSdk/Services/SepaDirectDebit/SepaDirectDebitPayWithEmandateRequest.cs
@@ -0,0 +1,18 @@
+﻿namespace BuckarooSdk.Services.SepaDirectDebit
+{
+    public class SepaDirectDebitPayWithEmandateRequest
+    {
+        /// <summary>
+        /// This is the MandateID retrieved from a successful Emandate request.
+        /// </summary>
+        public string MandateReference { get; set; }
+        /// <summary>
+        /// The date (dd-mm-yyyy) on which the direct debit should be collected from the consumer account. 
+        /// Important note: if combined with Credit Management, the collect date should always precede the due date of the invoice, since you don't want to trigger
+        /// a reminder step before debiting the customer. If however, the collect date is accidentally set after the due date, then the first reminder step will be 
+        /// postponed till the collect date, but only if it's set within 14 days after the due date. If it's set further than that, then our system will perform another 
+        /// check after 14 days and so on.
+        /// </summary>
+        public string CollectDate { get; set; }
+    }
+}
